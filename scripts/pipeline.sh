e6851e0dff9f1eaeedea57d21c8c3427de1cedea
carioka88 <rociorama88@gmail.com>
2020-08-03 19:37:54 +0200

    Updating pipeline and arguments to simulate data

---
+++ scripts/pipeline.sh
@@ -0,0 +1,6 @@
+path_coev='/Users/rramabal/Documents/PhD/Coevolution/Aminoacides/Simulated/CNN_project/DataSet_10/rawData/COEV/'
+path_no_coev='/Users/rramabal/Documents/PhD/Coevolution/Aminoacides/Simulated/CNN_project/DataSet_10/rawData/NO_COEV/'
+
+Rscript simulate_data.r $path_coev $path_no_coev 20 40 15 2 
+
+
--- scripts/simulate_data.r
+++ scripts/simulate_data.r
@@ -1,8 +1,11 @@
 source('/Users/rramabal/Documents/PhD/Coevolution/CNN_project/scripts/simulate_functions.r')
 
-output_path <- '/Users/rramabal/Documents/PhD/Coevolution/Aminoacides/Simulated/CNN_project/'
-output_path_coev <- paste(output_path,'DataSet_10/rawData/COEV/',sep="")
-output_path_no_coev <- paste(output_path,'DataSet_10/rawData/NO_COEV/',sep="")
+args <- commandArgs(trailingOnly = TRUE)
+output_path_coev <- args[1]
+output_path_no_coev <- args[2]
+msa_length <- strtoi(args[3])
+num_species <- strtoi(args[4])
+tree_length <- strtoi(args[5])
+num_simulations <- strtoi(args[6])
 
-
-simulate_all_data(output_path_coev, output_path_no_coev, 20, 40, 15, 2)
+simulate_all_data(output_path_coev, output_path_no_coev, msa_length, num_species, tree_length, num_simulations)
